080fdb7113151af93ba5f655e0bd185b3f914b0b
Ghazala Siyad <ghazalasiyad96@gmail.com>
2025-02-06 13:20:58 +0500

    pick and drop slider done

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,25 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import HomePageLayout from './components/HomePageLayout/HomePageLayout'
 import Home from './pages/Home/Home'
-import Pick from './pages/Pick_Drop/Pick-drop'
+// import Pick from './pages/Pick_Drop/Pick-drop'
 // import VehicleBooking from './components/Tabs/Tabs'
-// import Travel from './pages/Tour/Travel'
+// import Tour from './pages/Tour/Tour'
+import Partner from './pages/Partner/Partner'
 
 function App() {
   return (
     <>
     {/* <VehicleBooking/> */}
     {/* <Travel/> */}
-    <Pick/>
+    {/* <Pick/> */}
+    <Partner/>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<HomePageLayout/>}>
           <Route path='/' element={<Home/>}/>
+          {/* <Route path='partner' element={<Partner/>}/>
+          <Route path='tour' element={<Tour/>}/>
+          <Route path='pick' element={<Pick/>}/> */}
           
           </Route>
         </Routes>
