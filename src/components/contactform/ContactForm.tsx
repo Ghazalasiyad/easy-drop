882ceb71b461ebb91fa50d6fd3d5ab300e9bba2e
rayan472 <rayanaly000@gmial.com>
2025-08-06 12:09:36 +0500

    main

--- src/components/contactform/ContactForm.tsx
+++ src/components/contactform/ContactForm.tsx
@@ -49,7 +49,7 @@ const ContactForm: React.FC = () => {
                         <h1 className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-[#2E2C80] to-[#2458A4] mb-6">
                             Get In Touch
                         </h1>
-                        <div className="relative overflow-hidden pb-[56.25%] h-0 rounded-sm">
+                        {/* <div className="relative overflow-hidden pb-[56.25%] h-0 rounded-sm">
                             <iframe
                                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d532.7021887128724!2d74.38769913843461!3d35.91979544364983!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38e649b604ba8633%3A0x6bfa22d5c992633d!2sEasydrop%20travel!5e0!3m2!1sen!2s!4v1754462862666!5m2!1sen!2s"
                                 width="600"
@@ -59,7 +59,7 @@ const ContactForm: React.FC = () => {
                                 loading="lazy"
                                 referrerPolicy="no-referrer-when-downgrade"
                             />
-                        </div>
+                        </div> */}
 
                     </div>
 
