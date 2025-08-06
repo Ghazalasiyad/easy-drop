8c7ef5e4b9f294cd3031894dfab40514d9c65b1f
rayan472 <rayanaly000@gmial.com>
2025-08-06 11:55:46 +0500

    add the correct link of google map

--- src/components/contactform/ContactForm.tsx
+++ src/components/contactform/ContactForm.tsx
@@ -51,15 +51,16 @@ const ContactForm: React.FC = () => {
                         </h1>
                         <div className="relative overflow-hidden pb-[56.25%] h-0 rounded-sm">
                             <iframe
-                                src="https://www.google.com/maps/embed?...your map link..."
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d532.7021887128724!2d74.38769913843461!3d35.91979544364983!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38e649b604ba8633%3A0x6bfa22d5c992633d!2sEasydrop%20travel!5e0!3m2!1sen!2s!4v1754462862666!5m2!1sen!2s"
                                 width="600"
                                 height="450"
                                 style={{ border: 0 }}
-                                allowFullScreen
+                                allowFullScreen={true}
                                 loading="lazy"
                                 referrerPolicy="no-referrer-when-downgrade"
                             />
                         </div>
+
                     </div>
 
                     <div>
